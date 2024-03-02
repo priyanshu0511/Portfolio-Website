5d8d5fcc179f07244df3b28ecc129cb11e451557
Priyanshu Singh <priyanshu05112@gmail.com>
2024-03-02 20:32:06 +0530

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -8,7 +8,7 @@ function SocialLinks() {
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
             <li className='flex justify-between items-center px-4 w-40 h-14 bg-black ml-[-100px] hover:ml-0 duration-300'>
-                <a className='flex justify-between items-center text-white w-full' href="https:github.com/priyanshu0511" target="_blank" rel="noopener noreferrer">Github<FaGithub size={25}/></a>
+                <a className='flex justify-between items-center text-white w-full' href="https://github.com/priyanshu0511" target="_blank" rel="noopener noreferrer">Github<FaGithub size={25}/></a>
             </li>
             <li className='flex justify-between items-center px-4 w-40 h-14 bg-black ml-[-100px] hover:ml-0 duration-300'>
                 <a className='flex justify-between items-center text-white w-full' href="https://www.linkedin.com/in/priyanshu-singh-770401260/" target="_blank" rel="noopener noreferrer">LinkedIn<FaLinkedin size={25}/></a>
